0d3496f3d67f348d70b494e50da673c117c21635
Ramon Rivas <rivasdiaz@gmail.com>
2017-11-18 22:16:17 -0500

    Functional Components

--- src/main/kotlin/tictactoe/Square.kt
+++ src/main/kotlin/tictactoe/Square.kt
@@ -3,27 +3,10 @@ package tictactoe
 import kotlinx.html.js.onClickFunction
 import org.w3c.dom.events.Event
 import react.RBuilder
-import react.RComponent
-import react.RProps
-import react.RState
 import react.dom.button
 
-class Square(): RComponent<Square.Props, RState>() {
-
-    override fun RBuilder.render() {
-        button(classes = "square") {
-            +(props.value ?: "")
-            attrs.onClickFunction = props.onClickFunction
-        }
+fun RBuilder.square(value: String?, onClickFunction: (Event) -> Unit) =
+    button(classes = "square") {
+        +(value ?: "")
+        attrs.onClickFunction = onClickFunction
     }
-
-    interface Props: RProps {
-        var value: String?
-        var onClickFunction: (Event) -> Unit
-    }
-}
-
-fun RBuilder.square(value: String?, onClickFunction: (Event) -> Unit) = child(Square::class) {
-    attrs.value = value
-    attrs.onClickFunction = onClickFunction
-}
